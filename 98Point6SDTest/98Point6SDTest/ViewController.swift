0dd5d91cc4b174a3e8cdfd34c5e28ba3d41226a3
Stan Rosenbaum <ForeverTangent@gmail.com>
2021-02-17 08:50:03 -0500

    Init Commit

---
+++ 98Point6SDTest/98Point6SDTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  98Point6SDTest
+//
+//  Created by Stanley Rosenbaum on 2/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
